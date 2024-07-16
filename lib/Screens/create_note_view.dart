ff81abe8a88420541e5c86e5557db5e4e4161ba2
Liman <saniahmadliman@gmail.com>
2024-07-16 03:45:22 -0800

    done

--- lib/Screens/create_note_view.dart
+++ lib/Screens/create_note_view.dart
@@ -6,6 +6,7 @@ class CreateNoteView extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final TextEditingController titleController = TextEditingController();
+    final TextEditingController noteController = TextEditingController();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Create Note'),
@@ -32,6 +33,33 @@ class CreateNoteView extends ConsumerWidget {
                   width: double.infinity,
                   child: TextField(
                     controller: titleController,
+                    decoration: const InputDecoration(
+                      hintText: 'Your note here',
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                const Text(
+                  'Note',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  height: 50,
+                  width: double.infinity,
+                  child: TextField(
+                    controller: noteController,
                   ),
                 ),
               ],
