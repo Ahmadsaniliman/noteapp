3c91554340384253ae435ed0da97c3de759b7de8
Liman <saniahmadliman@gmail.com>
2024-07-22 03:22:05 -0800

    noy done

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:myfirstnotebook/Screens/home.dart';
-import 'package:myfirstnotebook/Screens/register.dart';
 import 'package:myfirstnotebook/firebase_options.dart';
 
 void main() async {
@@ -36,11 +35,3 @@ class MyApp extends StatelessWidget {
 }
 
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
