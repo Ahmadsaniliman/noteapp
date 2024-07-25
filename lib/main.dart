b38cb881f472cd4272515c68372ea32e28158c07
Liman <saniahmadliman@gmail.com>
2024-07-25 13:09:53 -0800

    noy done

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:myfirstnotebook/Screens/home.dart';
+import 'package:myfirstnotebook/Screens/login.dart';
+import 'package:myfirstnotebook/Screens/register.dart';
 import 'package:myfirstnotebook/firebase_options.dart';
 
 void main() async {
@@ -28,9 +30,18 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           useMaterial3: true,
         ),
-        home: const HomePage(),
+        home:  LoginScreen(),
       ),
     );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
 
